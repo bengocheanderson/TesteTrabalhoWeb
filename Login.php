6ddebc5a9ebe54a85aab2233ecb6e1ce46aabe63
Anderson Bengoche <bengocha@gmail.com>
2019-12-03 02:29:48 -0300

    Trabalho ok incrementando login

---
+++ Login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="estilologin.css">
+  </head>
+  <body>
+    <div id="corpoform">
+
+
+    <h2>Realizar Login</h2>
+    <form action="processa.php" method="POST">
+      <input type="email" placeholder="Usuario">
+      <input type="password" placeholder="Senha">
+      <input type="submit" name="" value="Logar">
+      <a href="">Realize seu cadastro para acessar o sistema</a>
+    </form>
+
+
+    </div>
+  </body>
+</html>
